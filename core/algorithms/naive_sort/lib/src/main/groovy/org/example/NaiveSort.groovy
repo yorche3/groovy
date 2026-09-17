5a885d956cbd80ac029e1c0563faefcc42363a18
yorche3 <hyaoki123@gmail.com>
2026-09-16 19:28:31 -0600

    feat(algorithms): add naive sort implementation

--- core/algorithms/naive_sort/lib/src/main/groovy/org/example/NaiveSort.groovy
+++ core/algorithms/naive_sort/lib/src/main/groovy/org/example/NaiveSort.groovy
@@ -14,5 +14,55 @@
 package org.example
 
 class NaiveSort {
-    
+    static List<Integer> selectionSort(List<Integer> list) {
+        if (list == null) return null
+        List<Integer> arr = list.clone()
+        for (int i = 0; i < arr.size() - 1; i++) {
+            int minIndex = i
+            for (int j = i + 1; j < arr.size(); j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j
+                }
+            }
+            if (minIndex != i) {
+                int temp = arr[i]
+                arr[i] = arr[minIndex]
+                arr[minIndex] = temp
+            }
+        }
+        return arr
+    }
+
+    static List<Integer> bubbleSort(List<Integer> list) {
+        if (list == null) return null
+        List<Integer> arr = list.clone()
+        boolean swapped
+        do {
+            swapped = false
+            for (int i = 0; i < arr.size() - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    int temp = arr[i]
+                    arr[i] = arr[i + 1]
+                    arr[i + 1] = temp
+                    swapped = true
+                }
+            }
+        } while (swapped)
+        return arr
+    }
+
+    static List<Integer> insertionSort(List<Integer> list) {
+        if (list == null) return null
+        List<Integer> arr = list.clone()
+        for (int i = 1; i < arr.size(); i++) {
+            int key = arr[i]
+            int j = i - 1
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j]
+                j--
+            }
+            arr[j + 1] = key
+        }
+        return arr
+    }
 }
